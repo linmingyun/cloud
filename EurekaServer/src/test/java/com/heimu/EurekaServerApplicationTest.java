93e518dfe5da2ed41855e2d98109b9a779651209
dongfangbai <813762318@qq.com>
2020-01-29 14:28:43 +0800

    spring cloud 入门

--- EurekaServer/src/test/java/com/heimu/EurekaServerApplicationTest.java
+++ EurekaServer/src/test/java/com/heimu/EurekaServerApplicationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EurekaserverApplicationTests {
+public class EurekaServerApplicationTest {
 
 	@Test
 	public void contextLoads() {
